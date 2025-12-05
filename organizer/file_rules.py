033f7e8877ce3ebe91ae9568a4c78dea672bc3e0
parth <parthparwani9@gmail.com>
2025-12-05 19:26:13 +0530

    Create file_rules.py

---
+++ organizer/file_rules.py
@@ -0,0 +1,7 @@
+FILE_RULES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mov"],
+    "Archives": [".zip", ".rar"],
+    "Code": [".py", ".js", ".html", ".css", ".json"]
+}
